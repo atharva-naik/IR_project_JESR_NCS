cf96da72868c339a59223c7f985ec4b9ac3f7f20
atharva-naik <atharvanaik2018@gmail.com>
2023-05-10 10:35:45 +0530

    missing checkpoint detection script

---
+++ scripts/verify_all_ckpts.py
@@ -0,0 +1,16 @@
+# check if all checkpoints are present
+import os
+import json
+
+# main
+if __name__ == "__main__":
+    ckpt_info = json.load(open("./checkpoint_info.json"))
+    ctr = 0
+    tot = len(ckpt_info)
+    for path, value in ckpt_info:
+        if not value: continue
+        model_path = os.path.join(path, "model.pt")
+        if os.path.exists(model_path):
+            print(f"\x1b[32;1mcheckpoint exists for {path}\x1b[0m")
+        else: print(f"\x1b[32;1mcheckpoint missing for {path}\x1b[0m")
+    print(f"missing {ctr}/{tot} checkpoints!")
